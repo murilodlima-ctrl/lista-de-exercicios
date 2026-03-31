acef72deb58f82380bc9688915c5b60da7566b24
murilodlima-ctrl <murilo.dlima@sou.unaerp.edu.br>
2026-03-31 20:56:33 -0300

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &a);
+
+    printf("Digite o segundo numero: ");
+    scanf("%d", &b);
+
+    return 0;
+}
