f9b01041c54e89b3409cde499dee4adbc6c08ff6
murilodlima-ctrl <murilo.dlima@sou.unaerp.edu.br>
2026-03-31 20:56:49 -0300

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, soma;
+
+    printf("Digite dois numeros: ");
+    scanf("%d %d", &a, &b);
+
+    soma = a + b;
+
+    printf("Soma = %d\n", soma);
+
+    return 0;
+}
